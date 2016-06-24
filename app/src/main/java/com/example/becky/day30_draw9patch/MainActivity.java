d2772126d68f54154f82757c136581a52c20794b
becky11392839 <940530542@qq.com>
2016-06-24 16:32:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/becky/day30_draw9patch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.becky.day30_draw9patch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
